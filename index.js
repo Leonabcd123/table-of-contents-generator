9b32f18354ec0aebe6730c06237600d7828a94f2
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-03-11 09:55:26 +0200

    impr(indentation): better indentation

--- index.js
+++ index.js
@@ -1,39 +1,75 @@
 function getStartingIndex() {
-  return document.getElementById("startingIndex").value || 0;
+  return parseInt(document.getElementById("startingIndex").value) || 0;
+}
+
+class Node {
+  parentNode;
+  level;
+  header;
+  constructor(parentNode, level, header) {
+    this.parentNode = parentNode;
+    this.level = level;
+    this.header = header;
+  }
+}
+
+function getIndentationLevel(currentNode, currentHeader) {
+  if (currentHeader.length === currentNode.header.length) {
+    return [currentNode, currentNode.level];
+  }
+
+  if (currentHeader.length > currentNode.header.length) {
+    const newLevel = currentNode.level + 1;
+    return [new Node(currentNode, newLevel, currentHeader), newLevel];
+  }
+
+  let node = currentNode;
+  while (node.parentNode) {
+    if (currentHeader.length >= node.header.length) {
+      const newLevel = node.level;
+      return [new Node(node, newLevel, currentHeader), newLevel];
+    }
+
+    node = node.parentNode;
+  }
+
+  return [new Node(null, 0, currentHeader), 0];
 }
 
 function genTableOfContents() {
-  const input = document.getElementById("input").value; 
+  const input = document.getElementById("input").value;
   const headersRegex = /^(#{1,6}) (.*)/gm;
   const titlesCount = new Map();
   const startingIndex = getStartingIndex();
-  let baseLevel = 0;
+  let node = null;
+  let indentationLevel = 0;
   let i = 0;
   let tableOfContents = "";
- 
-  const matches = input.matchAll(headersRegex); 
+
+  const matches = input.matchAll(headersRegex);
 
   for (match of matches) {
-   if (startingIndex <= i) {
-     if (baseLevel === 0) {
-       baseLevel = match[1].length;
-     }
-   
-     let title = match[2].toLowerCase().replaceAll(" ", "-");
-     const appearenceCount = titlesCount.get(title);
+    if (startingIndex <= i) {
+      let title = match[2].toLowerCase().replaceAll(" ", "-");
+      const appearenceCount = titlesCount.get(title);
 
-     if (appearenceCount) {
-       title += `-${appearenceCount}`;
-     }
+      if (appearenceCount) {
+        title += `-${appearenceCount}`;
+      }
 
-     titlesCount.set(title, (appearenceCount ?? 0) + 1);
-     const indentationLevel = match[1].length - baseLevel * 3;
+      titlesCount.set(title, (appearenceCount ?? 0) + 1);
 
-     tableOfContents += `${" ".repeat(indentationLevel > 0 ? indentationLevel : 0)}1. [${match[2]}](#${title})\n`;
-   }
+      if (i === startingIndex) {
+        node = new Node(null, 0, match[1]);
+      }
 
-   i++;
-  }; 
+      [node, indentationLevel] = getIndentationLevel(node, match[1]);
+
+      tableOfContents += `${" ".repeat(indentationLevel * 3)}1. [${match[2]}](#${title})\n`;
+    }
+
+    i++;
+  }
 
   document.getElementById("result").value = tableOfContents;
 }
